82e02ace357d2bd16ba0c2c005d37e4f50aa5e82
ruturaj-cloud <ingaler790@gmail.com>
2024-12-19 21:17:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ this file contains the deployment script..
 
 this line updating in master branch
 updating this file in dev branch
+
+updating this file to see the difference in git fetch command and git merge command
