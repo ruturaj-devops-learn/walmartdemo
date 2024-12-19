5c8694299273c1d715f733b38819af248c720cda
ruturaj-cloud <ingaler790@gmail.com>
2024-12-19 21:22:46 +0530

    updated for the add hoc commands

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ this file contains the deployment script..
 this line updating in master branch
 updating this file in dev branch
 
-updating this file to see the difference in git fetch command and git merge command
+updating this file to see the difference in git fetch command and git merge command 
+
+I am updating this to see execute the add hoc commands..
